c8d914e33a44ff0e5225322e44e73a99f297dce8
Johannes Schiel <j.schiel@unleashed-design.de>
2022-03-13 13:16:54 +0100

    create

---
+++ angular-reusable-button/src/app/shared/components/custom-button/custom-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-button',
+  templateUrl: './custom-button.component.html',
+  styleUrls: ['./custom-button.component.scss']
+})
+export class CustomButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- angular-reusable-button/src/app/shared/shared.module.ts
+++ angular-reusable-button/src/app/shared/shared.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CustomButtonComponent } from './components/custom-button/custom-button.component';
 
 @NgModule({
-    declarations: [],
+    declarations: [
+        CustomButtonComponent
+    ],
     imports: [
         CommonModule
     ],
-    exports: []
+    exports: [
+        CustomButtonComponent
+    ]
 })
 export class SharedModule { }
